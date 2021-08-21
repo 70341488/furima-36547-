52581cfac55a32e6fde42c2d922b25c4ddc5366a
70341488 <88077407+70341488@users.noreply.github.com>
2021-08-21 11:39:47 +0900

    正規表現の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
   with_options presence: true do
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
-    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: 'Full-width characters' }
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: 'Full-width characters' }
     validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Namekana kana Full-width katakana characters' }
     validates :family_name_kana,
               format: { with: /\A[ァ-ヶー－]+\z/, message: 'Family_namekana kana Full-width katakana characters' }
