8ae8cfaea9f6f6612088d26274f55dc9a5f661ea
70341488 <88077407+70341488@users.noreply.github.com>
2021-08-21 22:46:35 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,13 @@
 class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
+
+  validates :item_name, presence: true
+  validates :explanation, presence: true
+  validates :price, presence: true
+  validates :item_condition_id, presence: true
+  validates :delivery_fee_id, presence: true
+  validates :delivery_zone_id, presence: true
+  validates :category_id, presence: true
+  validates :delivery_time_id, presence: true
 end
