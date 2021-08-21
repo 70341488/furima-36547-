ce167f325709b74e23bcd6e22f5a17e9031785b2
70341488 <88077407+70341488@users.noreply.github.com>
2021-08-21 21:30:43 +0900

    Activehashのモデル作成とアソシエーション設定

--- app/models/itemselect.rb
+++ app/models/itemselect.rb
@@ -1,2 +1,14 @@
 class Itemselect < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+  belongs_to :condition
+  belongs_to :delivery
+  belongs_to :prefectures
+  belongs_to :time
+
+  validates :title, :text, presence: true
+
+  validates :genre_id, numericality: { other_than: 1 } 
+
+  validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
