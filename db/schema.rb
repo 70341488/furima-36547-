3e7bfcc021d265d04f7f3d551922d3f819082039
70341488 <88077407+70341488@users.noreply.github.com>
2021-08-21 17:32:36 +0900

    itemsデータベース作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_18_111354) do
+ActiveRecord::Schema.define(version: 2021_08_21_081205) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "item_name", null: false
+    t.text "explanation", null: false
+    t.integer "item_condition_id", null: false
+    t.integer "delivery_fee_id", null: false
+    t.integer "delivery_zone_id", null: false
+    t.integer "category_id", null: false
+    t.integer "delivery_time_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +44,5 @@ ActiveRecord::Schema.define(version: 2021_08_18_111354) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
