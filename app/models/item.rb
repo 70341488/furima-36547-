582766835792f5d28a9939f4ebf74f82108beca2
70341488 <88077407+70341488@users.noreply.github.com>
2021-08-22 16:45:07 +0900

    itemモデルにバリデーションを貼り付ける

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,4 +10,24 @@ class Item < ApplicationRecord
   validates :delivery_zone_id, presence: true
   validates :category_id, presence: true
   validates :delivery_time_id, presence: true
+
+  end
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_condition
+  belongs_to :delivery_fee
+  belongs_to :delivery_zone
+  belongs_to :delivery_time
+
+
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_condition, numericality: { other_than: 1 } 
+  validates :item_condition, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_fee, numericality: { other_than: 1 } 
+  validates :delivery_fee, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_zone, numericality: { other_than: 1 } 
+  validates :delivery_zone, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_time, numericality: { other_than: 1 } 
+  validates :delivery_time, numericality: { other_than: 1 , message: "can't be blank"}
 end
