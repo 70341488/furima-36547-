a7116c78b0ecf1fc22c39ecab4122909b99d2716
70341488 <88077407+70341488@users.noreply.github.com>
2021-08-23 11:25:13 +0900

    itemsモデルのバリデーション編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
 
   validates :item_name, presence: true
   validates :explanation, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, inclusion: { in: 300..9_999_999 }, numericality: { with: /\A[0-9]+\z/ }
   validates :item_condition_id, presence: true
   validates :delivery_fee_id, presence: true
   validates :delivery_zone_id, presence: true
@@ -19,14 +19,9 @@ class Item < ApplicationRecord
   belongs_to :delivery_time
 
 
-  validates :category_id, numericality: { other_than: 1 } 
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :item_condition_id, numericality: { other_than: 1 } 
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :item_condition_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :delivery_fee_id, numericality: { other_than: 1 } 
   validates :delivery_fee_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :delivery_zone_id, numericality: { other_than: 1 } 
-  validates :delivery_zone_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :delivery_time_id, numericality: { other_than: 1 } 
-  validates :delivery_time_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_zone_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :delivery_time_id, numericality: { other_than: 1 , message: "can't be blank"} 
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
+    item_name         {'ビール'}
+    explanation         {'新鮮なビール'}
+    price                   {5000}
+    item_condition_id   {2}
+    delivery_fee_id       {2}
+    delivery_zone_id      {2}
+    category_id             {2}
+    delivery_time_id        {2}
     
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
