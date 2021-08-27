635a53f50d297ccf60b1811b24c5d45fc8853e23
70341488 <88077407+70341488@users.noreply.github.com>
2021-08-27 11:13:45 +0900

    テストコードのファクトリーボット編集

---
+++ spec/factories/purchase_residences.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :purchase_residence do
+    municipalities {'東京都'}
+    address {'1-1'}
+    post_number {'123-4567'}
+    phone_number {'09012345678'}
+    delivery_zone_id { 1 }
+    building_name {'東京ハイツ'}
+  end
+end
