d0bc6a48e67586f084cb958f89145296bffd25e2
70341488 <88077407+70341488@users.noreply.github.com>
2021-08-27 17:37:09 +0900

    ここまでの単体テストコード終了

--- spec/factories/purchase_residences.rb
+++ spec/factories/purchase_residences.rb
@@ -4,9 +4,8 @@ FactoryBot.define do
     address {'1-1'}
     post_number {'123-4567'}
     phone_number {'09012345678'}
-    delivery_zone_id { 1 }
+    delivery_zone_id { 2 }
     building_name {'東京ハイツ'}
     item_id { 1 }
-    purchase_id { 1 }
   end
 end
--- spec/models/purchase_residence_spec.rb
+++ spec/models/purchase_residence_spec.rb
@@ -15,7 +15,59 @@ RSpec.describe PurchaseResidence, type: :model do
         @purchase_residence.building_name = ''
         expect(@purchase_residence).to be_valid
       end
+      end
 
-    end
+      context '内容に問題がある場合' do
+        it 'post_numberが空だと保存できないこと' do
+          @purchase_residence.post_number = ''
+          @purchase_residence.valid?
+          expect(@purchase_residence.errors.full_messages).to include("Post number can't be blank", "Post number is invalid")
+        end
+        it 'post_numberが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
+          @purchase_residence.post_number = '1234567'
+          @purchase_residence.valid?
+          expect(@purchase_residence.errors.full_messages).to include("Post number is invalid")
+        end
+        it 'delivery_zone_idを選択していないと保存できないこと' do
+          @purchase_residence.delivery_zone_id = 1
+          @purchase_residence.valid?
+          expect(@purchase_residence.errors.full_messages).to include("Delivery zone can't be blank")
+        end
+        it 'municipalitiesが空だと保存できないこと' do
+          @purchase_residence.municipalities = ''
+          @purchase_residence.valid?
+          expect(@purchase_residence.errors.full_messages).to include("Municipalities can't be blank")
+        end
+        it 'addressがないと保存できないこと' do
+          @purchase_residence.address = ''
+          @purchase_residence.valid?
+          expect(@purchase_residence.errors.full_messages).to include("Address can't be blank")
+        end
+        it 'phone_numberないと保存できないこと' do
+          @purchase_residence.phone_number = ''
+          @purchase_residence.valid?
+          expect(@purchase_residence.errors.full_messages).to include("Phone number can't be blank")
+        end
+        it 'phone_numberは半角数値でないと保存できないこと' do
+          @purchase_residence.phone_number = '４５４ひsdw'
+          @purchase_residence.valid?
+          expect(@purchase_residence.errors.full_messages).to include("Phone number is invalid")
+        end
+        it 'phone_numberは10桁以上じゃないと保存できないこと' do
+          @purchase_residence.phone_number = 999999999
+          @purchase_residence.valid?
+          expect(@purchase_residence.errors.full_messages).to include("Phone number is invalid")
+        end
+        it 'phone_numberは11桁以下じゃないと保存できないこと' do
+          @purchase_residence.phone_number = 999999999999
+          @purchase_residence.valid?
+          expect(@purchase_residence.errors.full_messages).to include("Phone number is invalid")
+        end
+        it 'userが紐付いていないと保存できないこと' do
+          @purchase_residence.user_id = nil
+          @purchase_residence.valid?
+          expect(@purchase_residence.errors.full_messages).to include("User can't be blank")
+        end
+      end
     end
 end
